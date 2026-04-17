4071b725b498d61e5bc2b590cc22feb95300db5e
UMAR ABUBAKAR <umarkhalifaabubakar0@gmail.com>
2026-04-17 02:20:22 +0100

    After failure

--- api/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('../server/src/app');
-const connectDB = require('../server/src/config/db');
-
-module.exports = async (req, res) => {
-  await connectDB();
-  return app(req, res);
-};
